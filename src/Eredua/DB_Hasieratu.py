e478efe0bb91a965d8a942d481cd0a852df69292
JonAnderDS <jonanderduk@gmail.com>
2025-01-18 21:21:57 +0100

    Datu Basea sortu funtzionatzen

--- src/Eredua/DB_Hasieratu.py
+++ src/Eredua/DB_Hasieratu.py
@@ -21,24 +21,25 @@ def init_db():
                 """)
         
         c.execute("""
-                    CREATE TABLE IF NOT EXISTS film(
+                    CREATE TABLE IF NOT EXISTS pelikula(
                     id_pelikula int primary key,
                     izena varchar(50),
                     deskribapena varchar(500),
                     alokairuKopurua int,
-                    iruzkinKopurua int,
+                    iruzkinKopurua int
                 )
                 """)
         
         c.execute("""
                     CREATE TABLE IF NOT EXISTS alokairua(
                     id_alokairua int primary key,
-                    nan varchar(9) primary key,
-                    id_pelikula int primary key,
-                    hasieraData date primary key,
+                    nan varchar(9) ,
+                    id_pelikula int ,
+                    hasieraData date ,
                     amaieraData date ,
                     foreign key (nan) references erabiltzailea(nan),
-                    foreign key (id_pelikula) references film(id_pelikula)
+                    foreign key (id_pelikula) references pelikula(id_pelikula)
+                    unique (nan, id_pelikula, hasieraData)
                 )
                 """)
         
--- src/Eredua/Pelikula.py
+++ src/Eredua/Pelikula.py
@@ -1,5 +1,5 @@
 import sqlite3
-class Filma:
+class Pelikula:
     def __init__(self, db_name="datubase.db"):
         self.db_name = db_name
     
